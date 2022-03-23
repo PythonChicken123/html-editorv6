df0544517c5c91526bb34e725bdb59bb882ca401
NaboKabbo <101510622+PythonChicken123@users.noreply.github.com>
2022-03-23 22:56:32 +0000

    Add files via upload

---
+++ public_useage/UnlimitedAhmet.js
@@ -0,0 +1,7 @@
+alert("Welcome to The Ahmet's Fart Wiki!  Ahmet's Unlimited Farted Script is Active And Availible for FREE!")
+
+function GetChangeButton() {
+  let x = document.getElementById("ot-sdk-btn");
+  x.style.fontSize = "25px"; 
+  x.style.color = "red"; 
+}
